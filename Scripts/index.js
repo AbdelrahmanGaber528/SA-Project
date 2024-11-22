0d92ddd9792f6d441581981a794f32f83fcaa7ad
AbdelrahmanGaber528 <144827977+AbdelrahmanGaber528@users.noreply.github.com>
2024-11-22 06:52:24 +0200

    add in test branch

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,10 +1,30 @@
 
-// Toggle Dropdown Menu
+//  Dropdown Menu Categories
+
+/***************************************************************** */
+/***************************************************************** */
+
 function Dropdown(event) {
+
     event.preventDefault();
+    /*
+    stops the browser from navigating to #.
+    */
+    
     const dropdown = event.target.closest('.dropdown');
+    /*ou ensure that only the dropdown related to the clicked element is toggled,
+     not all dropdowns on the page. */
+    
     dropdown.classList.toggle('active');
 }
+
+/***************************************************************** */
+/***************************************************************** */
+
+/** Sidebar funcitons  */
+
+/***************************************************************** */
+/***************************************************************** */
 
 function openSidebar() {
     const side = document.getElementById("sidebar");
@@ -14,6 +34,10 @@ function openSidebar() {
 function closeSidebar() {
     document.getElementById("sidebar").style.width = "0";
 }
+
+/***************************************************************** */
+/***************************************************************** */
+
 
 // Close dropdown  if clicked outside
 
